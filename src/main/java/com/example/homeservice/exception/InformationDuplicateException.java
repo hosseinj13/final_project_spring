c5a14ffae689cfc90e8f2eec5ce4ea7511f3749d
hossein javadi <hosseinj13@yahoo.com>
2024-06-14 17:20:02 +0330

    Create InformationDuplicateException.java

---
+++ src/main/java/com/example/homeservice/exception/InformationDuplicateException.java
@@ -0,0 +1,8 @@
+package com.example.homeservice.exception;
+
+public class InformationDuplicateException extends RuntimeException{
+
+    public InformationDuplicateException(String message) {
+        super(message);
+    }
+}
