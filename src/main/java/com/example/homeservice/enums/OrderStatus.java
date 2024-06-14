6ba0c198e443c584186894afa994d10d3f3a12f1
hossein javadi <hosseinj13@yahoo.com>
2024-06-14 17:17:59 +0330

    Create OrderStatus.java enumeration.

---
+++ src/main/java/com/example/homeservice/enums/OrderStatus.java
@@ -0,0 +1,10 @@
+package com.example.homeservice.enums;
+
+public enum OrderStatus {
+    WAITING_FOR_SPECIALIST_OFFER,
+    WAITING_FOR_SPECIALIST_SELECTION,
+    WAITING_FOR_SPECIALIST_ARRIVAL,
+    STARTED,
+    DONE,
+    PAID
+}
